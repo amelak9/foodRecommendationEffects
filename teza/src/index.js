65d2b4f0a36d8e972ced693e3f9f21bdebb0897a
amelak9 <amela.kurtic@student.tugraz.at>
2022-12-22 20:43:18 +0100

    db

--- teza/src/index.js
+++ teza/src/index.js
@@ -1,83 +1,76 @@
 import React from "react";
 import express from 'express';
 import bodyParser from 'body-parser';
-import connection from 'express-myconnection';
 import mysql from 'mysql';
-/*
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path='/' element={< Homepage />}></Route>
-                <Route exact path='/0' element={< NoEffect />}></Route>
-                <Route exact path='/1' element={< ExplanationPositionEffect />}></Route>
-                <Route exact path='/2' element={< Explanation />}></Route>
-                <Route exact path='/3' element={< PositionEffect />}></Route>
-                <Route exact path='/4' element={< DecoyEffect />}></Route>
-                <Route exact path='/5' element={< ExplanationDecoyEffect />}></Route>
-                <Route exact path='/6' element={< DecoyPositionEffect />}></Route>
-                <Route exact path='/7' element={< DecoyPositionExplanation />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
-}
-root.render(<App tab="home"/>);*/
-
 import path from 'path';
 const __dirname = path.resolve();
 const app = express();
+
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/pages/'));
 
-app.use(
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: '3306'
+});
 
-    connection(mysql,{
+con.connect(function(err) {
+    if (err) throw err
+});
 
-        host: 'localhost', //'localhost',
-        user: 'admin',
-        password : 'password',
-        port : 3306, //port mysql
-        database:'results'
-
-    },'pool')); //or single
+con.query("CREATE DATABASE recipes")
 
 app.post('/add_result',(req,res)=>{
 
-    let {recipe_num,result_val,user_id} = req.body;
+    const request = req.body;
+    let recipe1 = request.recipe1;
+    let recipe2 = request.recipe2;
+    let recipe3 = request.recipe3;
+    let recipe4 = request.recipe4;
+    let recipe5 = request.recipe5;
+    let recipe6 = request.recipe6;
+    if(recipe1 == null) return res.status(400).json('Recipe cant be blank');
+    if(recipe2 == null) return res.status(400).json('Recipe cant be blank');
+    if(recipe3 == null) return res.status(400).json('Recipe cant be blank');
+    if(recipe4 == null) return res.status(400).json('Recipe cant be blank');
+    if(recipe5 == null) return res.status(400).json('Recipe cant be blank');
+    if(recipe6 == null) return res.status(400).json('Recipe cant be blank');
 
-    if(!recipe_num) return res.status(400).json('Recipe number cant be blank');
-    if(!result_val) return res.status(400).json('Result value cant be blank');
-    if(!user_id) return res.status(400).json('User id cant be blank');
+    const user_id = Math.random()
+    for (let i = 1; i < 7; i++) {
+        const data = {
+            recipe_num: i,
+            result_val: req.body[i - 1],
+            user_id: user_id
+        };
+        con.query("INSERT INTO results set ? ",data,
+            function(err, rows)
+            {
 
-    var data={recipe_num:recipe_num,
-        result_val:result_val,
-        user_id: user_id
-    };
+                if (err){
+                    //If error
+                    res.status(400).json('Sorry!!Unable To Add');
+                    console.log("Error inserting : %s ",err );
+                }
+                else
+                    //If success
+                    res.status(200).json('Result Added Successfully!!')
 
-
-    var query = connection.query("INSERT INTO results set ? ",data,
-        function(err, rows)
-        {
-
-            if (err){
-                //If error
-                res.status(400).json('Sorry!!Unable To Add');
-                console.log("Error inserting : %s ",err );
-            }
-            else
-                //If success
-                res.status(200).json('Result Added Successfully!!')
-
-        });
-
-
+            });
+    }
 });
 
-app.get('/',function(req,res) {
-    res.sendFile(__dirname + '/pages/homepage.html');
-});
+app.get('/',function(req,res) { res.sendFile(__dirname + '/pages/homepage.html'); });
+app.get('/0',function(req,res) { res.sendFile(__dirname + '/pages/NoEffect.html'); });
+app.get('/1',function(req,res) { res.sendFile(__dirname + '/pages/Explanation.html'); });
+app.get('/2',function(req,res) { res.sendFile(__dirname + '/pages/PositionEffect.html'); });
+app.get('/3',function(req,res) { res.sendFile(__dirname + '/pages/DecoyEffect.html'); });
+app.get('/4',function(req,res) { res.sendFile(__dirname + '/pages/DecoyPositionEffect.html'); });
+app.get('/5',function(req,res) { res.sendFile(__dirname + '/pages/DecoyPositionExplanation.html'); });
+app.get('/6',function(req,res) { res.sendFile(__dirname + '/pages/ExplanationDecoyEffect.html'); });
+app.get('/7',function(req,res) { res.sendFile(__dirname + '/pages/ExplanationPositionEffect.html'); });
 
 app.listen(8080, ()=> {
     console.log(`app is running on port 8080`);
