83f1e528deb6c3d04e7b58a6425f86f5b7f3125a
amelak9 <amela.kurtic@student.tugraz.at>
2023-01-18 15:48:04 +0100

    clear db setup

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ const __dirname = path.resolve();
 const app = express();
 import mysql from "mysql";
 
-var con=mysql.createPool({
+var con=mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB
-});
-
-con.connect(function(err) {
-    if (err) throw err
-    console.log("Connected!");
 });
 
 con.query("CREATE TABLE IF NOT EXISTS users (user_id VARCHAR(200)," +
