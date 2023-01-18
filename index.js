ca793d35657c853cf2d87d35cca811a3dfaac715
amelak9 <amela.kurtic@student.tugraz.at>
2023-01-18 15:54:11 +0100

    clear db setup

--- index.js
+++ index.js
@@ -6,12 +6,37 @@ const __dirname = path.resolve();
 const app = express();
 import mysql from "mysql";
 
-var con=mysql.createConnection({
+var db_config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB
-});
+};
+
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 con.query("CREATE TABLE IF NOT EXISTS users (user_id VARCHAR(200)," +
     "age INTEGER," +
