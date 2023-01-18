2db5661713250e1fd2ca6088873ef71e5323a374
amelak9 <amela.kurtic@student.tugraz.at>
2023-01-18 15:43:02 +0100

    clear db setup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const __dirname = path.resolve();
 const app = express();
 import mysql from "mysql";
 
-var con=mysql.createConnection({
+var con=mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
